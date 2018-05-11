e5081550fbedfbadf745f150bd44d97322307365
zhuhaiqing <zhuhaiqing@live.com>
2018-05-11 13:35:11 +0800

    initial docker node

---
+++ init_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+yum install -y deltarpm
+yum install -y update
+reboot
+sleep 60
+
+yum install -y docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://08a4uas2.mirror.aliyuncs.com","http://a95d9975.m.daocloud.io"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
